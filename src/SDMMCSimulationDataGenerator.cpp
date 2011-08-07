c7691d35f9e078feeec1f904e08303c1a3d814ee
Dirk Hörner <dirker@gmail.com>
2011-08-07 23:30:54 +0200

    Infinitly create CMD0's when simulating

--- src/SDMMCSimulationDataGenerator.cpp
+++ src/SDMMCSimulationDataGenerator.cpp
@@ -19,7 +19,7 @@ void SDMMCSimulationDataGenerator::Initialize(U32 simulation_sample_rate, SDMMCA
     mClock = mChannels.Add(mSettings->mClockChannel, mSimulationSampleRateHz, BIT_HIGH);
     mCommand = mChannels.Add(mSettings->mCommandChannel, mSimulationSampleRateHz, BIT_HIGH);
     
-    /* introduce a bit of idle time */
+    /* introduce a few periods of no clock */
     mChannels.AdvanceAll(mClockGenerator.AdvanceByHalfPeriod(10.0));
 }
 
@@ -27,13 +27,69 @@ U32 SDMMCSimulationDataGenerator::GenerateSimulationData(U64 newest_sample_reque
 {
     U64 adjusted_newest_sample_requested = AnalyzerHelpers::AdjustSimulationTargetSample( newest_sample_requested, sample_rate, mSimulationSampleRateHz );
 
-    /* FIXME: also generate traffic on the CMD line */
     while (mClock->GetCurrentSampleNumber() < adjusted_newest_sample_requested) {
-        mClock->Transition();
-        mChannels.AdvanceAll(mClockGenerator.AdvanceByHalfPeriod(.5));
+        CreateIdle(0.1);
+        CreateCommand();
     }
     
     *channels = mChannels.GetArray();
     return mChannels.GetCount();
 }
 
+void SDMMCSimulationDataGenerator::CreateClockPeriod()
+{
+    mClock->Transition();
+    mChannels.AdvanceAll(mClockGenerator.AdvanceByHalfPeriod(.5));
+    
+    mClock->Transition();
+    mChannels.AdvanceAll(mClockGenerator.AdvanceByHalfPeriod(.5));
+}
+
+void SDMMCSimulationDataGenerator::CreateIdle(double seconds)
+{
+    U64 samples = 400000.0 * seconds;
+    U64 orig = mClock->GetCurrentSampleNumber();
+    
+    while ((mClock->GetCurrentSampleNumber() - orig) < samples) {
+        CreateClockPeriod();
+    }    
+}
+
+void SDMMCSimulationDataGenerator::CreateCommand()
+{
+    /* make sure we continue with clock as high */
+    if (mClock->GetCurrentBitState() != BIT_HIGH) {
+        mClock->Transition();
+        mChannels.AdvanceAll(mClockGenerator.AdvanceByHalfPeriod(.5));
+    }
+    
+    /* start bit */
+    mCommand->TransitionIfNeeded(BIT_LOW);
+    CreateClockPeriod();
+    
+    /* host transfers */
+    mCommand->TransitionIfNeeded(BIT_HIGH);
+    CreateClockPeriod();
+    
+    /* index: 0 */
+    for (int i = 0; i < 6; i++) {
+        mCommand->TransitionIfNeeded(BIT_LOW);
+        CreateClockPeriod();
+    }
+    
+    /* argument: 0 */
+    for (int i = 0; i < 32; i++) {
+        mCommand->TransitionIfNeeded(BIT_LOW);
+        CreateClockPeriod();
+    }
+    
+    /* FIXME: calculate real crc7 */
+    for (int i = 0; i < 7; i++) {
+        mCommand->Transition();
+        CreateClockPeriod();
+    }
+    
+    /* stop bit */
+    mCommand->TransitionIfNeeded(BIT_HIGH);
+    CreateClockPeriod();
+}
--- src/SDMMCSimulationDataGenerator.h
+++ src/SDMMCSimulationDataGenerator.h
@@ -15,6 +15,11 @@ public:
     
     void Initialize(U32 simulation_sample_rate, SDMMCAnalyzerSettings *settings);
     U32 GenerateSimulationData(U64 newest_sample_requested, U32 sample_rate, SimulationChannelDescriptor **channels);
+
+protected:
+    void CreateClockPeriod();
+    void CreateIdle(double seconds);
+    void CreateCommand();
     
 protected:
     SDMMCAnalyzerSettings *mSettings;
